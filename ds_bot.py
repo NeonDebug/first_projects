662fc6409009fa09008eb9303092bb32bf0cb93f
NeonDebug <talmontasrr@gmail.com>
2021-11-21 13:19:29 +0200

    Ds bot

---
+++ ds_bot.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+
+
+
+bot = commands.Bot(command_prefix='!')
+
+bot.lava_nodes = [
+    {
+        "host": "lava.link",
+        "port": 80,
+        "rest_uri": f"http://lava.link:80",
+        "identifier": "MAIN",
+        'password': 'anything',
+        "region": "russia"
+    }
+]
+
+@bot.event
+async def on_ready():
+    print ("Bot gatov. ")
+    bot.load_extension('dismusic')
+
+bot.run('OTExOTA2MzMyOTAxMjY5NTA0.YZoM9A.EIwnioJhXvPXjKOaoIRkxi2_960')
